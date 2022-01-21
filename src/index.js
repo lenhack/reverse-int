760591923c8c4f8df3691252b2cdacab2866ad9f
Olena Koppalova <64489905+lenhack@users.noreply.github.com>
2022-01-21 20:41:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    const str = n.toString();
+    let reverseStr = '';
+    let i = 0;
+    if(str[i] === '-'){
+        i++;
+    }
+
+    while(i < str.length){
+        reverseStr = str[i] + reverseStr;
+        i++;
+    }
+
+    return +reverseStr;
 }
